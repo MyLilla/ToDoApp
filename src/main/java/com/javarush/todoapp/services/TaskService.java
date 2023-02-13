b4cb0efef3ae99ccc2784f7dcdaf32bc39463597
MyLilla <ninakarinaiok@gmail.com>
2023-02-13 19:06:28 +0200

    front styles task temp

---
+++ src/main/java/com/javarush/todoapp/services/TaskService.java
@@ -0,0 +1,4 @@
+package com.javarush.todoapp.services;
+
+public class TaskService {
+}
---
+++ src/main/java/com/javarush/todoapp/srvlets/TaskServlet.java
@@ -0,0 +1,23 @@
+package com.javarush.todoapp.srvlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TaskServlet", value = "/tasks")
+public class TaskServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
